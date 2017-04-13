googleapis/google-cloud-java

Add Identity Access Management (IAM) to the Storage API (#1812)
Adds support for bucket-level IAM (currently in limited alpha). More information
about IAM in Google Cloud Storage can be found at
https://cloud.google.com/storage/docs/access-control/iam

google-cloud-contrib/google-cloud-nio/src/test/java/com/google/cloud/storage/contrib/nio/FakeStorageRpc.java
@@ -19,7 +19,9 @@ package com.google.cloud.storage.contrib.nio;
 import com.google.api.services.storage.model.Bucket;
 import com.google.api.services.storage.model.BucketAccessControl;
 import com.google.api.services.storage.model.ObjectAccessControl;
+import com.google.api.services.storage.model.Policy;
 import com.google.api.services.storage.model.StorageObject;
+import com.google.api.services.storage.model.TestIamPermissionsResponse;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageException;
 import com.google.cloud.storage.spi.v1.RpcBatch;
@@ -61,6 +63,7 @@ import javax.annotation.concurrent.NotThreadSafe;
  *   <li>continueRewrite
  *   <li>createBatch
  *   <li>checksums, etags
+ *   <li>IAM operations</li>
  *   </ul>
  * </ul>
  */
@@ -442,5 +445,20 @@ class FakeStorageRpc implements StorageRpc {
     fakeFolder.setGeneration(so.getGeneration());
     folders.put(folderName, fakeFolder);
     return true;
+  }
+
+  @Override
+  public Policy getPolicy(String bucket) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Policy updatePolicy(String bucket, Policy policy) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public TestIamPermissionsResponse testPermissions(String bucket, List<String> permissions) {
+    throw new UnsupportedOperationException();
   }
 }

googleapis/java-storage-nio

google-cloud-nio/src/test/java/com/google/cloud/storage/contrib/nio/FakeStorageRpc.java
@@ -19,7 +19,9 @@ package com.google.cloud.storage.contrib.nio;
 import com.google.api.services.storage.model.Bucket;
 import com.google.api.services.storage.model.BucketAccessControl;
 import com.google.api.services.storage.model.ObjectAccessControl;
+import com.google.api.services.storage.model.Policy;
 import com.google.api.services.storage.model.StorageObject;
+import com.google.api.services.storage.model.TestIamPermissionsResponse;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageException;
 import com.google.cloud.storage.spi.v1.RpcBatch;
@@ -61,6 +63,7 @@ import javax.annotation.concurrent.NotThreadSafe;
  *   <li>continueRewrite
  *   <li>createBatch
  *   <li>checksums, etags
+ *   <li>IAM operations</li>
  *   </ul>
  * </ul>
  */
@@ -442,5 +445,20 @@ class FakeStorageRpc implements StorageRpc {
     fakeFolder.setGeneration(so.getGeneration());
     folders.put(folderName, fakeFolder);
     return true;
+  }
+
+  @Override
+  public Policy getIamPolicy(String bucket) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public Policy setIamPolicy(String bucket, Policy policy) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public TestIamPermissionsResponse testIamPermissions(String bucket, List<String> permissions) {
+    throw new UnsupportedOperationException();
   }
 }