1af03387938393ab57d272b5d4db0dd5c8aa1e51
mpeddada1 <mpeddada@google.com>
2024-08-20 22:18:03 +0000

    add -X

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -74,7 +74,7 @@ integration)
     ;;
 graalvm)
     # Run Unit and Integration Tests with Native Image
-    mvn test -Dsurefire.failIfNoSpecifiedTests=false "-Dtest=ITGcsNio#testCopy_replaceFile_withoutOption" -Djava.util.logging.config.file=src/main/resources/logging.properties -Pnative -Penable-integration-tests
+    mvn test -Dsurefire.failIfNoSpecifiedTests=false "-Dtest=ITGcsNio#testCopy_replaceFile_withoutOption" -Djava.util.logging.config.file=src/main/resources/logging.properties -Pnative -Penable-integration-tests -X
     RETURN_CODE=$?
     ;;
 graalvm17)
